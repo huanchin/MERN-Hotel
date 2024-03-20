3c3356cd398e47f33d0ec97c329a5ccd6a4a9de1
huan-chin chen <cherry032826@gmail.com>
2024-03-20 15:40:43 +0800

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route index element={<Home />} />
         <Route path="/signin" element={<Signin />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,42 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="bg-sky-800 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-sky-50">MERN</span>
+            <span className="text-sky-300">Hotel</span>
+          </h1>
+        </Link>
+        <form className="bg-sky-50 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-500" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-sky-50 hover:underline">
+              HOME
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-sky-50 hover:underline">
+              ABOUT
+            </li>
+          </Link>
+          <Link to="/signin">
+            <li className="text-sky-50 hover:underline">SIGN IN</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
