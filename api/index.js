14f4694c20bfa7908cbfd4710f618b8edce05274
huan-chin chen <cherry032826@gmail.com>
2024-03-20 16:08:31 +0800

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
+dotenv.config({ path: "./config.env" });
 
-app.listen(8080, () => {
-  console.log(`Server is running on port 8080`);
+const port = process.env.PORT || 3000;
+
+const Database = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(Database, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected successfully!"))
+  .catch((err) => {
+    console.log("💥ERROR", err);
+    console.log("Database connected fail!");
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
