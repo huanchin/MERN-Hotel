7f260830e05154afe669a78fe7a6d5e84acd1935
huan-chin chen <cherry032826@gmail.com>
2024-03-27 23:36:13 +0800

    create get user listings api route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/userModel.js";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listingModel.js";
 
 export const test = (req, res) => {
   res.json({
@@ -40,6 +41,17 @@ export const deleteUser = async (req, res, next) => {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie("access_token");
     res.status(200).json("User deleted successfully");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only view your own listings"));
+  try {
+    const listings = await Listing.find({ userRef: req.params.id });
+    res.status(200).json(listings);
   } catch (err) {
     next(err);
   }
